750b063ae5feee577728079f53c6c1ee26be6cc1
Muslim Mirzajonov <106766921+muslimmirzajonov@users.noreply.github.com>
2026-05-15 22:38:18 +0500

    Initial Commit

---
+++ Spinify/Localization/L10n.swift
@@ -0,0 +1,8 @@
+//
+//  L10n.swift
+//  Spinify
+//
+//  Created by Muslim Mirzajonov on 15/05/26.
+//
+
+import Foundation
---
+++ Spinify/Views/Setup/LanguagePickerSheet.swift
@@ -0,0 +1,8 @@
+//
+//  LanguagePickerSheet.swift
+//  Spinify
+//
+//  Created by Muslim Mirzajonov on 15/05/26.
+//
+
+import Foundation
